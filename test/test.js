2f9b90e51e4d2202e9ab2de5fcfb9761d5c5c8e5
luhuawei <2824054196@qq.com>
2023-03-05 20:46:42 +0800

    修改版本

--- test/test.js
+++ test/test.js
@@ -1,17 +1,22 @@
 "use strict"
 const expect = require("chai").expect
-const { deleteInvalid, deletePrimitiveInvalid, isEmpty } = require("../dist/index")
+const { isNotNegativeInteger } = require("../dist/index")
 
 describe("ts-hi function test", () => {
   it("should return 2", () => {
-    let obj = {
-      b: 0,
-      c: false,
-      d: null,
-      e: undefined,
-      f: [],
-      g: {},
-    }
-    console.log(deleteInvalid(obj), obj, "ooo")
+    let a = -1
+    let b = 0
+    let c = 1.1
+    let d = "1"
+    let e = "1.1"
+    let f = "wo"
+    let g = "0"
+    console.log(isNotNegativeInteger(a))
+    console.log(isNotNegativeInteger(b))
+    console.log(isNotNegativeInteger(c))
+    console.log(isNotNegativeInteger(d))
+    console.log(isNotNegativeInteger(e))
+    console.log(isNotNegativeInteger(f))
+    console.log(isNotNegativeInteger(g))
   })
 })
