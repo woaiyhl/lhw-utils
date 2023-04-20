df91ec453ec6a736af0f579d579d4b83218a9717
luhuawei <2824054196@qq.com>
2023-04-20 10:31:03 +0800

    添加递归删除无效属性

--- lib/object.ts
+++ lib/object.ts
@@ -1,11 +1,11 @@
 import { isEmpty } from './judge'
-import { cloneDeep } from 'lodash'
+import _,{ cloneDeep } from 'lodash'
 /**
  * http:www.woaini.com?name=lhw&age=12
  * 
  * getQueryObject(url)	 // { name: 'lhw',age: '12' }
  */
-export function getQueryObject(url: string | undefined):Record<string,string> {
+export function getQueryObject(url?: string | undefined):Record<string,string> {
   url = url == null ? window.location.href : url
   const search = url.substring(url.lastIndexOf('?') + 1)
   const obj = {}
@@ -27,7 +27,7 @@ export function getQueryObject(url: string | undefined):Record<string,string> {
  * ?page=1&size=2kg'
  *
  */
-export const objectToQueryString = (queryParameters: Record<string,any> | undefined):string => {
+export const objectToQueryString = (queryParameters?: Record<string,any> | undefined):string => {
   return queryParameters
     ? Object.entries(queryParameters).reduce((queryString, [key, val], index) => {
         const symbol = queryString.length === 0 ? '?' : '&';
@@ -66,4 +66,29 @@ export const deleteInvalid = (obj:Record<string,any>):Record<string,any> => {
 	    }
     })
 	return deepCloneObj;
+}
+
+/**
+ * 
+ * 传入一个对象，递归删除对象里面值为null、undefined,'',[],{} 的这些无效的字段
+ * 在业务中，数字0和布尔false属于有效值
+ */
+
+export const deleteInvalidDeep = (obj:Record<string,any>):Record<string,any> => {
+  return function prune(current) {
+    _.forOwn(current, function (value:any, key:string) {
+      if (_.isUndefined(value) || _.isNull(value) || _.isNaN(value) ||
+        (_.isString(value) && _.isEmpty(value)) ||
+        (_.isObject(value) && _.isEmpty(prune(value)))) {
+
+        delete current[key];
+      }
+    });
+    // remove any leftover undefined values from the delete 
+    // operation on an array
+    if (_.isArray(current)) _.pull(current, undefined);
+
+    return current;
+
+  }(_.cloneDeep(obj));  // Do not modify the original object, create a clone instead
 }
--- test/test.js
+++ test/test.js
@@ -1,22 +1,33 @@
 "use strict"
 const expect = require("chai").expect
-const { isNotNegativeInteger } = require("../dist/index")
+const { deleteInvalidDeep } = require("../dist/index")
 
 describe("ts-hi function test", () => {
   it("should return 2", () => {
-    let a = -1
-    let b = 0
-    let c = 1.1
-    let d = "1"
-    let e = "1.1"
-    let f = "wo"
-    let g = "0"
-    console.log(isNotNegativeInteger(a))
-    console.log(isNotNegativeInteger(b))
-    console.log(isNotNegativeInteger(c))
-    console.log(isNotNegativeInteger(d))
-    console.log(isNotNegativeInteger(e))
-    console.log(isNotNegativeInteger(f))
-    console.log(isNotNegativeInteger(g))
+    let obj = {
+      a: null,
+      b: undefined,
+      c: 0,
+      d: false,
+      e: NaN,
+      f: [],
+      g: {},
+      h: ['',1,'false',undefined,null,0],
+      i: {
+        a: null,
+        b: undefined,
+        c: 0,
+        d: false,
+        e: NaN,
+        f: [],
+        g: {},
+        h: 1,
+        i: {
+          a: null,
+          b: [{a:0}]
+        }
+      }
+    }
+    console.log(deleteInvalidDeep(obj),'ooo')
   })
 })
